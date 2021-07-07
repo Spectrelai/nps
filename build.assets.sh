2f163dad25115d11d1721ad00b3515cbf8d3f49e
Yorha <spectrelai@gmail.com>
2021-07-07 12:41:04 +0800

    Update build.assets.sh

--- build.assets.sh
+++ build.assets.sh
@@ -1,7 +1,7 @@
 export GOPROXY=direct
 
 sudo apt-get update
-sudo apt-get install gcc-mingw-w64-i686 gcc-mingw-w64-x86_64 gcc-multilib
+sudo apt-get install gcc-mingw-w64-i686 gcc-mingw-w64-x86-64 gcc-multilib
 env GOOS=windows GOARCH=386 CGO_ENABLED=1 CC=i686-w64-mingw32-gcc go build -ldflags "-s -w -extldflags -static -extldflags -static" -buildmode=c-shared -o npc_386_sdk.dll cmd/npc/sdk.go
 env GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc go build -ldflags "-s -w -extldflags -static -extldflags -static" -buildmode=c-shared -o npc_amd64_sdk.dll cmd/npc/sdk.go
 env GOOS=linux GOARCH=386 CGO_ENABLED=1 CC=gcc go build -ldflags "-s -w -extldflags -static -extldflags -static" -buildmode=c-shared -o npc_386_sdk.so cmd/npc/sdk.go
