d34937f9c389ea3f0cddd18ca2fe97fdb0b30369
Yorha <spectrelai@gmail.com>
2021-07-07 12:02:28 +0800

    Fix amd64 build

--- build.assets.sh
+++ build.assets.sh
@@ -6,7 +6,7 @@ env GOOS=windows GOARCH=386 CGO_ENABLED=1 CC=i686-w64-mingw32-gcc go build -ldfl
 env GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc go build -ldflags "-s -w -extldflags -static -extldflags -static" -buildmode=c-shared -o npc_amd64_sdk.dll cmd/npc/sdk.go
 env GOOS=linux GOARCH=386 CGO_ENABLED=1 CC=gcc go build -ldflags "-s -w -extldflags -static -extldflags -static" -buildmode=c-shared -o npc_386_sdk.so cmd/npc/sdk.go
 env GOOS=linux GOARCH=amd64 CGO_ENABLED=1 CC=gcc go build -ldflags "-s -w -extldflags -static -extldflags -static" -buildmode=c-shared -o npc_amd64_sdk.so cmd/npc/sdk.go
-tar -czvf npc_sdk.tar.gz npc_386_sdk.dll npc_amd64_sdk.dll npc_386_sdk.so npc_amd64_sdk.h
+tar -czvf npc_sdk.tar.gz npc_386_sdk.dll npc_amd64_sdk.dll npc_386_sdk.so npc_amd64_sdk.so npc_sdk.h
 
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w -extldflags -static -extldflags -static"  ./cmd/npc/npc.go
 
