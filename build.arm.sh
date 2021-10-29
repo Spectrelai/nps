f32f6c462c7a98cd052db18b19f95f350345fc5b
Yorha <spectrelai@gmail.com>
2021-10-29 11:27:00 +0800

    Update build.arm.sh

--- build.arm.sh
+++ build.arm.sh
@@ -1,4 +1,5 @@
 export GOPROXY=direct
+export CGO_ENABLED=1
 
-env GOOS=linux GOARCH=arm GOARM=7 CGO_ENABLED=1 CC=gcc go build -ldflags "-s -w -extldflags -static -extldflags -static" -buildmode=c-shared -o npc_arm_sdk.so cmd/npc/sdk.go
-tar -czvf npc_sdk.tar.gz npc_arm_sdk.so npc_arm_sdk.h
+go build -ldflags "-s -w -extldflags -static -extldflags -static" -buildmode=c-shared -o build/npc_arm_sdk.so cmd/npc/sdk.go
+tar -czvf npc_sdk.tar.gz build/npc_arm_sdk.so build/npc_arm_sdk.h
