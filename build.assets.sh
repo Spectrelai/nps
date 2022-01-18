90c49be436a09025bdedd4b6dc113cd296f119d6
Yorha <spectrelai@gmail.com>
2022-01-18 17:56:10 +0800

    Update build.assets.sh

--- build.assets.sh
+++ build.assets.sh
@@ -3,7 +3,7 @@ export GOPROXY=direct
 sudo apt-get update
 sudo apt-get install gcc-multilib gcc-mingw-w64-i686 gcc-mingw-w64-x86-64 
 sudo apt-get install gcc-arm-linux-gnueabihf
-sudo apt-get install gcc-mipsel-linux-gnu
+sudo apt-get install gcc-mips-linux-gnu
 
 env GOOS=windows GOARCH=386 CGO_ENABLED=1 CC=i686-w64-mingw32-gcc go build -buildmode=c-archive -o libnpc_386_sdk.lib cmd/npc/sdk.go
 env GOOS=windows GOARCH=386 CGO_ENABLED=1 CC=i686-w64-mingw32-gcc go build -ldflags "-s -w -extldflags -static -extldflags -static" -buildmode=c-shared -o libnpc_386_sdk.dll cmd/npc/sdk.go
@@ -13,6 +13,6 @@ tar -czvf npc_windows_sdk.tar.gz libnpc_386_sdk.dll libnpc_386_sdk.lib libnpc_38
 
 env GOOS=linux GOARCH=arm GOARM=7 CGO_ENABLED=1 CC=arm-linux-gnueabihf-gcc go build -buildmode=c-archive -o libnpc_arm_sdk.a cmd/npc/sdk.go
 env GOOS=linux GOARCH=arm GOARM=7 CGO_ENABLED=1 CC=arm-linux-gnueabihf-gcc go build -buildmode=c-shared -o libnpc_arm_sdk.so cmd/npc/sdk.go
-env GOOS=linux GOARCH=mipsle CGO_ENABLED=1 CC=mipsel-linux-gnu-gcc go build -buildmode=c-archive -o libnpc_mipsel_sdk.a cmd/npc/sdk.go
-env GOOS=linux GOARCH=mipsle CGO_ENABLED=1 CC=mipsel-linux-gnu-gcc go build -buildmode=c-shared -o libnpc_mipsel_sdk.so cmd/npc/sdk.go
-tar -czvf npc_linux_sdk.tar.gz libnpc_arm_sdk.a libnpc_arm_sdk.so libnpc_mipsel_sdk.a libnpc_mipsel_sdk.so libnpc_arm_sdk.h
+env GOOS=linux GOARCH=mips GOMIPS=softfloat CGO_ENABLED=1 CC=mips-linux-gnu-gcc go build -buildmode=c-archive -o libnpc_mips_sdk.a cmd/npc/sdk.go
+env GOOS=linux GOARCH=mips GOMIPS=softfloat CGO_ENABLED=1 CC=mips-linux-gnu-gcc go build -buildmode=c-shared -o libnpc_mips_sdk.so cmd/npc/sdk.go
+tar -czvf npc_linux_sdk.tar.gz libnpc_arm_sdk.a libnpc_arm_sdk.so libnpc_arm_sdk.h libnpc_mips_sdk.a libnpc_mips_sdk.so libnpc_mips_sdk.h
